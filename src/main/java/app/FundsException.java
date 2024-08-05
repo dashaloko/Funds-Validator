6800ae6101a3e94e49d0c874a895ba9fa4816cbc
Daria <dashalokomoko30@gmail.com>
2024-08-05 10:21:07 -0400

    Fixed errors

--- src/main/java/app/FundsException.java
+++ src/main/java/app/FundsException.java
@@ -1,6 +1,6 @@
 package app;
 
-// Кастомний виняток
+
 public class FundsException extends Throwable {
 
     public FundsException(String message) {
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,12 +8,12 @@ public class Main {
 
 
     public static void main(String[] args) {
-
-        balance = validateAmount(balance, getAmount());
+     balance = getBalance();
+      balance = validateAmount(balance, getAmount());
     }
 
     private static double getBalance() {
-        return 1000.00; // Наявні кошти на рахунку
+        return 1000.00;
     }
 
     private static double getAmount() {
@@ -23,9 +23,10 @@ public class Main {
         return scanner.nextDouble();
     }
 
-    // Метод валідації наявних коштів
-    private static void validateAmount(double balance, double withdrawal) {
+
+    private static double validateAmount(double balance, double withdrawal) {
         if (withdrawal > balance) {
+
             try {
                 throw new FundsException("Insufficient funds!");
             } catch (FundsException ex) {
@@ -35,11 +36,10 @@ public class Main {
             balance = getBalance(balance, withdrawal);
             System.out.printf("Funds are OK. Purchase paid." +
                     "%nBalance is USD %.2f", balance);
-        }
+        }return balance;
     }
 
-    // Метод розрахунку наявних коштів на рахунку
-    // після зняття певної суми коштів
+
     private static double getBalance(double balance, double withdrawal) {
         return balance - withdrawal;
     }
